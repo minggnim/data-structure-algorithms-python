b57af8693dce287c045c314792d6eaf143c1d4f0
mg <ming_gao@outlook.com>
2020-10-24 18:14:39 +0100

    housekeeping

---
+++ test_staircase_solution.py
@@ -0,0 +1,25 @@
+import pytest
+
+@pytest.fixture(scope="module")
+def solution():
+    from stair_case_problem import staircaseSolution
+
+    return staircaseSolution()
+
+test_data = [(-1, 0),
+             (0, 1),
+             (1, 1),
+             (2, 1),
+             (3, 2),
+             (4, 3),
+             (5, 5),
+             (6, 8)
+             ]
+
+@pytest.mark.parametrize("test_input, expected", test_data)
+def test_recursive_solution(solution, test_input, expected):
+    assert solution.calc_num_ways_recursive(test_input) == expected
+
+@pytest.mark.parametrize("test_input, expected", test_data)
+def test_iterative_solution(solution, test_input, expected):
+    assert solution.calc_num_ways_iterative(test_input) == expected
