113a6f12f0c2052e698d543f967803c8f0b9fd5f
minggnim <38852608+minggnim@users.noreply.github.com>
2021-09-10 07:54:33 +0100

    update the recursion module

--- recursion.py
+++ recursion.py
@@ -1,9 +1,10 @@
 from typing import List
 
+
 class reverseList(object):
     def __init__(self, l: List[str]):
         self.l = l
-    
+
     def reverse_list(self):
         self.l.reverse()
 
@@ -12,12 +13,12 @@ class reverseList(object):
 
     def reverse_list_iter(self):
         for i in range(len(self.l) // 2):
-            self.l[i], self.l[-i-1] = self.l[-i-1], self.l[i]
+            self.l[i], self.l[-i - 1] = self.l[-i - 1], self.l[i]
 
     def reverse_list_rec(self):
-        def rec_func(left=0, right=len(self.l)-1):
+        def rec_func(left=0, right=len(self.l) - 1):
             if left < right:
                 self.l[left], self.l[right] = self.l[right], self.l[left]
                 rec_func(left + 1, right - 1)
+
         rec_func()
-    
--- tests/test_recursion.py
+++ tests/test_recursion.py
@@ -1,17 +1,19 @@
 import pytest
 from recursion import reverseList
 
-@pytest.fixture(scope='module')
+
+@pytest.fixture(scope="module")
 def test_case():
-    return {'input': [1,2,3], 'output': [[1,2,3], [3,2,1]]}
+    return {"input": [1, 2, 3], "output": [[1, 2, 3], [3, 2, 1]]}
+
 
 def test_reverse_list(test_case):
-    rl = reverseList(test_case['input'])
+    rl = reverseList(test_case["input"])
     rl.reverse_list()
-    assert rl.l == test_case['output'][1]
+    assert rl.l == test_case["output"][1]
     rl.reverse_list_idx()
-    assert rl.l == test_case['output'][0]
+    assert rl.l == test_case["output"][0]
     rl.reverse_list_iter()
-    assert rl.l == test_case['output'][1]
+    assert rl.l == test_case["output"][1]
     rl.reverse_list_rec()
-    assert rl.l == test_case['output'][0]
+    assert rl.l == test_case["output"][0]
