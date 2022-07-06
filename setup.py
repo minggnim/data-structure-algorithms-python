3523a3c8a6f6797fd4ca7fb36abf39a5a51b5d1e
minggnim <38852608+minggnim@users.noreply.github.com>
2022-07-06 22:56:44 +0100

    package project

--- setup.py
+++ setup.py
@@ -1,8 +1,9 @@
 from setuptools import find_packages, setup
 
 setup(
-    name='data-structure',
-    packages=find_packages(),
+    name='data-struct',
+    packages=find_packages('src.data_struct'),
+    package_dir={'': 'src'},
     version='0.1.0',
     description='Data Structure and Algorithms',
     author='ming_gao@outlook.com',
